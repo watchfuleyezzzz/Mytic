e3ef318036ef14fdc979bc680cfe741aa22c230c
Raven <oscar.braberg@hotmail.com>
2018-05-04 16:58:44 +0200

    Started working on UI.

--- Mytic/MainForm.Designer.cs
+++ Mytic/MainForm.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.Elipse = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.MenuSeparator = new Bunifu.Framework.UI.BunifuSeparator();
+            this.MenuPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Elipse
+            // 
+            this.Elipse.ElipseRadius = 5;
+            this.Elipse.TargetControl = this;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(85)))), ((int)(((byte)(147)))));
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(800, 40);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.MenuPanel.Controls.Add(this.MenuSeparator);
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 40);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(135, 410);
+            this.MenuPanel.TabIndex = 1;
+            // 
+            // MenuSeparator
+            // 
+            this.MenuSeparator.BackColor = System.Drawing.Color.Transparent;
+            this.MenuSeparator.LineColor = System.Drawing.SystemColors.ScrollBar;
+            this.MenuSeparator.LineThickness = 1;
+            this.MenuSeparator.Location = new System.Drawing.Point(125, 0);
+            this.MenuSeparator.Name = "MenuSeparator";
+            this.MenuSeparator.Size = new System.Drawing.Size(18, 405);
+            this.MenuSeparator.TabIndex = 2;
+            this.MenuSeparator.Transparency = 255;
+            this.MenuSeparator.Vertical = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MenuPanel);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.Text = "Mytic";
+            this.MenuPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse Elipse;
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Panel TopPanel;
+        private Bunifu.Framework.UI.BunifuSeparator MenuSeparator;
     }
 }
 
